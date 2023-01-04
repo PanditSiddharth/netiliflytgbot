81935228eeb06d2db1b71d312aa8e7cea059cdb2
Siddharth Sharma <75090218+sidhelpbot@users.noreply.github.com>
2023-01-04 21:22:34 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const TelegramBot = require('node-telegram-bot-api')
+
+const token = process.env.BOT_TOKEN
+const port = process.env.PORT
+const host = process.env.HOST
+const bot = new TelegramBot(token, {webHook: {port, host}})
+bot.setWebHook(https://${process.env.URL}/bot${token})
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, 'Welcome to my bot!')
+})
+
+bot.onText(/\/help/, (msg) => {
+  bot.sendMessage(msg.chat.id, 'Send me a message and I will echo it back!')
+})
+
+bot.on('message', (msg) => {
+  bot.sendMessage(msg.chat.id, Echo: ${msg.text})
+})
